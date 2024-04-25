dce0e377e73d83e4ac03399c2c3a7a51e7af6132
Oscar Felix Cota <135076646+BTF53@users.noreply.github.com>
2024-04-25 10:51:15 -0700

    Conexión a SQL

--- Proyecto/backend/app.js
+++ Proyecto/backend/app.js
@@ -6,10 +6,39 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+// Conexion a MySQL - cadena
+let conexion = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//No Conectamos a MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    } else{
+        console.log('Conectado a a base de datos');
+    }
+});
+
 // Rutas de acceso
 app.get("/", function(req,res){
     res.send("Ruta de inicio");
 })
+
+// Seleccionamos todos los clientes
+app.get('/api/clientes',(req, res)=>{
+    conexion.query('SELECT * FROM CLIENTES', (error, filas)=>{
+        if(error){
+            throw error;
+        } else{
+            res.send(filas);
+        }
+    });
+});
 
 // Encender servidor
 let puerto = 3000;
