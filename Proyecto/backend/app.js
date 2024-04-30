ed60901f19e3e644f8f5845b04b379710f9025a2
Oscar Felix Cota <135076646+BTF53@users.noreply.github.com>
2024-04-30 10:56:40 -0700

    Borrar e insertar

--- Proyecto/backend/app.js
+++ Proyecto/backend/app.js
@@ -40,6 +40,62 @@ app.get('/api/clientes',(req, res)=>{
     });
 });
 
+// Seleccionamos un cliente en especifico 
+app.get('/api/clientes/:id', (req, res)=>
+{
+    conexion.query('SELECT * FROM CLIENTES WHERE ID = ?', (req, params, id), (error, fila)=>
+    {
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {
+            res.send(fila);
+        }
+    })
+});
+
+app.delete('/api/clientes/:id', (req,res)=>
+{
+    let id = req.params.id;
+    conexion.query('DELETE FROM CLIENTES WHERE ID = ?', [id], (error, filas)=>
+    {
+        if(error)
+        {
+            throw error
+        }
+        else
+        {
+            res.send(filas)
+        }
+    })
+})
+
+// Insertar un nuevo cliente
+app.post('/api/clientes', (req, res)=>
+{
+    let data = {
+        id:req.body.id,
+        nombre:req.body.nombre,
+        apellido:req.body.apellido,
+        direccion:req.body.direccion,
+        telefono:req.body.telefono,
+        rfc:req.body.rfc,
+        curp:req.body.curp,
+        cp:req.body.cp
+    }
+    let sql = "INSERT INTO CLIENTES SET ?";
+    conexion.query(sql,data,(error,resultado)=>{
+        if(error){
+            throw error;
+        } else {
+            res.send(resultado);
+        }
+
+    })
+})
+
 // Encender servidor
 let puerto = 3000;
 app.listen(puerto, function(){
